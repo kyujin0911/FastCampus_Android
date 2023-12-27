57a75989e92510359e77d87fef685687cd1ab00b
KKJ <kyujin0911@tukorea.ac.kr>
2023-12-28 01:06:37 +0900

    Part2_Ch3 Socket을 활용하여 Client 앱 만들기

--- Part2/part2chapter3/part2chapter3test/app/src/main/java/umc/mission/part2chapter3test/MainActivity.kt
+++ Part2/part2chapter3/part2chapter3test/app/src/main/java/umc/mission/part2chapter3test/MainActivity.kt
@@ -8,6 +8,7 @@ import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.HttpURLConnection
 import java.net.ServerSocket
+import java.net.Socket
 
 // HttpURLConnection은 내부적으로 okhttp를 사용
 // okhttp를 사용하면 소켓 프로그래밍을 직접 안해도 되서 편리함
@@ -16,6 +17,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread{
+            try {
+                // 애뮬레이터의 로컬호스트를 사용하기 위해 10.0.0.2 사용
+                val socket = Socket("10.0.2.2", 8080)
+                val printer = PrintWriter(socket.getOutputStream())
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+                printer.println("GET / HTTP/1.1")
+                printer.println("Host: 192.168.219.116:8080")
+                printer.println("User-Agent: android")
+                printer.println("\r\n")
+                printer.flush()
+
+                var input: String? = "-1"
+                while (input != null) {
+                    input = reader.readLine()
+                    Log.e("Client", input)
+                }
+                reader.close()
+                printer.close()
+                socket.close()
+            } catch (e: Exception){
+                Log.e("CLIENT", e.toString())
+            }
+        }.start()
 
     }
 }
