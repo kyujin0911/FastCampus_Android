5923c894153c8ba76fd79a931c5a8e7713f9b32d
KKJ <kyujin0911@tukorea.ac.kr>
2024-01-04 21:04:07 +0900

    Terminal Part3_Ch2 보안키패드 - UI 구성 1

---
+++ Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/PinViewModel.kt
@@ -0,0 +1,30 @@
+package umc.mission.part3chapter2
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PinViewModel: ViewModel() {
+    val passwordLiveData : LiveData<CharSequence> by lazy { _passwordLiveData }
+    private val _passwordLiveData by lazy { MutableLiveData<CharSequence>() }
+
+    private val password: StringBuffer = StringBuffer("")
+
+    fun input(num: String){
+        if(password.length < 6){
+            password.append(num)
+            _passwordLiveData.value = password.toString()
+        }
+    }
+
+    fun delete(){
+        if(password.isNotEmpty()){
+            password.deleteCharAt(password.length - 1) // index는 0부터 시작하므로
+            _passwordLiveData.value = password.toString()
+        }
+    }
+
+    fun done(){
+        password.replace(0, password.length, "")
+    }
+}
