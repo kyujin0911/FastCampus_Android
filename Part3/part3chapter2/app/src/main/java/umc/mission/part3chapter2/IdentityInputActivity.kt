e54983ce4c2aab24a9aaaed9ef394cef83a00dd2
KKJ <kyujin0911@tukorea.ac.kr>
2024-01-04 22:57:41 +0900

    Part3_Ch2 휴대폰 본인 인증 UI 구성 2

--- Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/IdentityInputActivity.kt
+++ Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/IdentityInputActivity.kt
@@ -1,8 +1,15 @@
 package umc.mission.part3chapter2
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import umc.mission.part3chapter2.databinding.ActivityIdentityInputBinding
+import umc.mission.part3chapter2.util.ViewUtil.hideKeyboard
+import umc.mission.part3chapter2.util.ViewUtil.setOnEditorActionListener
+import umc.mission.part3chapter2.util.ViewUtil.showKeyboard
+import umc.mission.part3chapter2.util.ViewUtil.showKeyboardDelay
 
 class IdentityInputActivity: AppCompatActivity() {
 
@@ -13,5 +20,50 @@ class IdentityInputActivity: AppCompatActivity() {
         binding = ActivityIdentityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.view = this
+        initView()
+        binding.nameEdit.showKeyboardDelay()
+    }
+
+    private fun initView(){
+        with(binding){
+            nameEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT){
+                birthdayLayout.isVisible = true
+                birthdayEdit.showKeyboard()
+            }
+
+            birthdayEdit.doAfterTextChanged {
+                if(birthdayEdit.length() > 7){
+                    genderLayout.isVisible = true
+                    birthdayEdit.hideKeyboard()
+                }
+            }
+
+            genderChipGroup.setOnCheckedStateChangeListener{ group, checkIds ->
+                if(!telecomLayout.isVisible){
+                    telecomLayout.isVisible = true
+                }
+            }
+
+            telelcomChipGroup.setOnCheckedStateChangeListener{ group, checkIds ->
+                if(!phoneLayout.isVisible){
+                    phoneLayout.isVisible = true
+                    phoneEdit.showKeyboard()
+                }
+            }
+
+            phoneEdit.doAfterTextChanged {
+                if(phoneEdit.length() > 10){
+                    confirmBtn.isVisible = true
+                    phoneEdit.hideKeyboard()
+                }
+            }
+
+            phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE){
+                if(phoneEdit.length() > 9){
+                    confirmBtn.isVisible = true
+                    phoneEdit.hideKeyboard()
+                }
+            }
+        }
     }
 }
---
+++ Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/util/ViewUtil.kt
@@ -0,0 +1,35 @@
+package umc.mission.part3chapter2.util
+
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import umc.mission.part3chapter2.util.ViewUtil.showKeyboard
+
+object ViewUtil {
+    fun EditText.setOnEditorActionListener(action: Int, invoke: () -> Unit){
+        setOnEditorActionListener { _, actionId, _ ->
+            if(actionId == action){
+                invoke()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    fun EditText.showKeyboard(){
+        this.requestFocus()
+        val inputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    fun EditText.showKeyboardDelay(){
+        postDelayed({showKeyboard()}, 200) // 람다 함수를 사용해 함수를 Runnalbe 객체로 변환 가능
+    }
+
+    fun EditText.hideKeyboard(){
+        this.clearFocus()
+        val inputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+    }
+}
