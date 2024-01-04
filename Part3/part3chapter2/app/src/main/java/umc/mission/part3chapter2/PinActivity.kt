d57e4b46face9c7e38179651fcafeaaee6436301
KKJ <kyujin0911@tukorea.ac.kr>
2024-01-04 21:48:36 +0900

    Part3_Ch2 보안키패드 - 데이터 무결성 체크

--- Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/PinActivity.kt
+++ Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/PinActivity.kt
@@ -1,6 +1,7 @@
 package umc.mission.part3chapter2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import umc.mission.part3chapter2.databinding.ActivityPinBinding
@@ -19,6 +20,10 @@ class PinActivity: AppCompatActivity(), ShuffleNumberKeyboard.KeyPadListener {
         binding.lifecycleOwner = this
 
         binding.shuffleKeyboard.setKeyPadListener(this)
+
+        viewModel.messageLiveData.observe(this){
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onClickNum(num: String) {
