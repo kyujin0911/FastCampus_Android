e1b38dbb2f4180e17ced00e5fb266b8f7579b531
KKJ <kyujin0911@tukorea.ac.kr>
2024-01-08 23:22:40 +0900

    Part3_Ch2 휴대폰 본인 인증 UI 화면 구성(인증번호 입력) 2 Countdown Timer

--- Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/VerifyOtpActivity.kt
+++ Part3/part3chapter2/app/src/main/java/umc/mission/part3chapter2/VerifyOtpActivity.kt
@@ -1,11 +1,21 @@
 package umc.mission.part3chapter2
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import umc.mission.part3chapter2.databinding.ActivityVerifyOtpBinding
 
 class VerifyOtpActivity: AppCompatActivity() {
     private lateinit var binding: ActivityVerifyOtpBinding
+    private var timer: CountDownTimer? = object : CountDownTimer((3* 60 * 1000), 1000){
+        override fun onTick(p0: Long) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onFinish() {
+            TODO("Not yet implemented")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVerifyOtpBinding.inflate(layoutInflater)
